26de574044a05258ba8e05610356f5b6dab2a2b2
bsovs <brandon.sovran@gmail.com>
2021-02-02 19:02:13 -0500

    DC-2 Cors Update

--- services/api/api.service.js
+++ services/api/api.service.js
@@ -28,13 +28,13 @@ module.exports = {
 		// Global CORS settings for all routes
 		cors: {
 			// Configures the Access-Control-Allow-Origin CORS header.
-			origin: "*",
+			origin: "https://drawingchallenge5.web.app/",
 			// Configures the Access-Control-Allow-Methods CORS header.
-			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			methods: ["GET", "POST"],
 			// Configures the Access-Control-Allow-Headers CORS header.
 			allowedHeaders: [],
 			// Configures the Access-Control-Expose-Headers CORS header.
-			exposedHeaders: [],
+			exposedHeaders: ["Authorization"],
 			// Configures the Access-Control-Allow-Credentials CORS header.
 			credentials: false,
 			// Configures the Access-Control-Max-Age CORS header.
